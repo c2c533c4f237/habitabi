87eb45360c9d10b0aee1f2fc3a2cba4244226933
Stacia <git.stacia@xoxy.net>
2014-02-12 16:16:56 -0800

    Added rest of files, oops.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Habitabi::Application.config.secret_key_base = '9818f522fb9f0d99e56b053ca60b0ff711936a013b0cdb83feae41af3ddd8ac0fa2ab94136b21840f5ec0e21e8f0c4bb111bbce6de5a4009752a5c057de12473'
