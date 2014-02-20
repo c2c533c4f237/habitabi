738c2cebdfbfe94eb43e0d915dd2ab3101658c1e
Stacia <git.stacia@xoxy.net>
2014-02-19 19:36:05 -0800

    Added custom welcome controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Habitabi::Application.routes.draw do
+  get "welcome/index"
   resources :habits
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
- root 'habits#index'
+ root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
