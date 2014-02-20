729a797553018ee83925a3692350b246d15183ef
Stacia <git.stacia@xoxy.net>
2014-02-19 19:30:09 -0800

    Added saved scope

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,3 +1,24 @@
 class Habit < ActiveRecord::Base
 	belongs_to :user
+
+	def self.categoricals
+		where(habit_type: 0)
+	end
+
+	def self.events
+		where(habit_type: 1)
+	end
+
+	def self.counters
+		where(habit_type: 2)
+	end
+
+	def self.measurements
+		where(habit_type: 3)
+	end
+
+	def self.amounts
+		where(habit_type: 4)
+	end
+	
 end
