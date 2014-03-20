a22a7d51bcbe92738161dadb2690f39dfe98d6b9
Stacia <git.stacia@xoxy.net>
2014-03-19 19:48:08 -0700

    CanCan bug fix from https://github.com/ryanb/cancan/issues/835#issuecomment-18663815

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => exception.message
+  	redirect_to root_url, :alert => exception.message
   end
+
+  before_filter do
+  	resource = controller_name.singularize.to_sym
+  	method = "#{resource}_params"
+  	params[resource] &&= send(method) if respond_to?(method, true)
+  end
+  
 end
