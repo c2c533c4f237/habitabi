b9fe19d7c97219b27f6f329f75deafdf78e5ca49
Stacia <git.stacia@xoxy.net>
2014-03-24 18:09:31 -0700

    updated

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -44,9 +44,9 @@ class Habit < ActiveRecord::Base
     #Takes in a string and attemps to turn it into a habit.
     def self.parse_habit_text(text)
 
-    	habit_parse_regex = /(\w+)(?:\s+(\d+(?:\s+))?(?:(\d+)([a-zA-Z]+)(?:\s+)?)?([\w\s]+)*)?/
+    	
         
-        return HabitText.new(*text.scan(habit_parse_regex).flatten)
+        return HabitText.new(text)
      
     end
 	
@@ -78,15 +78,42 @@ end
 
 class HabitText
 	attr_accessor :action_name, :count, :quantity, :quantity_type, :value_name, :measurement, :measurement_type, :habit_type
-	def initialize(action_name, count, num, num_type, value_name)
-		@action_name = action_name
-		@count = count
-		@quantity = num
-		@quantity_type = num
-		@value_name = value_name
+	def initialize(text)
+		habit_parse_regex = 
+					/(\w+)(?:\s+(\d+(?:\s+))?(?:(\d+)([a-zA-Z]+)(?:\s)*)?([\w\s]+)*)?/
+		@action_name, @count, @quantity, @quantity_type, @value_name = 
+						*text.scan(habit_parse_regex).flatten
+
+		@habit_type = 0 if self.action_only?
+		@habit_type = 1 if self.action_and_value_only?
+		@habit_type = 2 if self.action_and_value_and_count?
+	    if self.action_and_number_and_no_value?
+	    	@measurement, @measurement_type = @quantity, @quantity_type
+	    	@quantity, @quantity_type = nil, nil
+	    	@habit_type = 3
+	    end
+	    @habit_type = 4 if self.action_and_value_and_quantity?
 	end
 
 	def action_only?
-     @action_name and !(@count and @quantity and @quantity_type and @value_name)
+     @action_name and @count.nil? and @quantity.nil? and @quantity_type.nil? and @value_name.nil?
 	end
+
+	def action_and_value_only?
+     @action_name and @value_name and @count.nil? and @quantity.nil? and @quantity_type.nil?
+	end
+
+	def action_and_value_and_count?
+     @action_name and @value_name and @count and @quantity.nil? and @quantity_type.nil?
+	end
+
+	def action_and_number_and_no_value?
+     @action_name and @value_name.nil? and @count.nil? and @quantity and @quantity_type
+	end
+
+
+	def action_and_value_and_quantity?
+     @action_name and @value_name and @quantity and @quantity_type
+	end
+
 end
