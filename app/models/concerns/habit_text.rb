c59ea4f85ee6d3aa52195f06cc24f6090a5e0769
Stacia <git.stacia@xoxy.net>
2014-03-25 17:40:02 -0700

    Fixed some logic issues in creating habits from text

--- app/models/concerns/habit_text.rb
+++ app/models/concerns/habit_text.rb
@@ -2,12 +2,12 @@ class HabitText
 	attr_accessor :action_name, :count, :quantity, :quantity_type, :value_name, :measurement, :measurement_type, :habit_type
 	def initialize(text)
 		habit_parse_regex = 
-		/(\w+)(?:\s+(\d+(?:\s+))?(?:(\d+)([a-zA-Z]+)(?:\s)*)?([\w\s]+)*)?/
+		/(\w+)(?:\s+(\d+)?(?:\s)*(?:(\d+)([a-zA-Z]+)(?:\s)*)?([\w\s]+)*)?/
 		@action_name, @count, @quantity, @quantity_type, @value_name = 
 		*text.scan(habit_parse_regex).flatten
-		@value_name = @value_name.singularize if @value_name
-		@count = @count.to_i if @count
-		@quantity = @quantity.to_f if @quantity
+		@value_name = @value_name.singularize if @value_name.present?
+		@count = @count.to_i if @count.present?
+		@quantity = @quantity.to_f if @quantity.present?
 
 		@habit_type = 0 if self.action_only?
 		@habit_type = 1 if self.action_and_value_only?
@@ -21,23 +21,23 @@ class HabitText
 	end
 
 	def action_only?
-		@action_name and @count.nil? and @quantity.nil? and @quantity_type.nil? and @value_name.nil?
+		@action_name.present? and @count.nil? and @quantity.nil? and @quantity_type.nil? and @value_name.nil?
 	end
 
 	def action_and_value_only?
-		@action_name and @value_name and @count.nil? and @quantity.nil? and @quantity_type.nil?
+		@action_name.present? and @value_name.present? and @count.nil? and @quantity.nil? and @quantity_type.nil?
 	end
 
 	def action_and_value_and_count?
-		@action_name and @value_name and @count and @quantity.nil? and @quantity_type.nil?
+		@action_name.present? and @value_name.present? and @count.present? and @quantity.nil? and @quantity_type.nil?
 	end
 
 	def action_and_number_and_no_value?
-		@action_name and @value_name.nil? and @count.nil? and @quantity and @quantity_type
+		@action_name.present? and @value_name.nil? and @count.present? and @quantity.nil? and @quantity_type.nil?
 	end
 
 	def action_and_value_and_quantity?
-		@action_name and @value_name and @quantity and @quantity_type
+		@action_name.present? and @value_name.present? and @quantity.present? and @quantity_type.present?
 	end
 
 end
