e8d21978a9019f678c234dae7dcaef81602679ae
Stacia <git.stacia@xoxy.net>
2014-03-26 18:48:43 -0700

    Fixing habit factory/habit text bugs

--- app/models/concerns/habit_factory.rb
+++ app/models/concerns/habit_factory.rb
@@ -1,4 +1,5 @@
 class HabitFactory
+
 	def self.save_habit(habit)
 		if _action = Action.where("name = '#{habit.action.name}' AND user_id = #{habit.user_id}").first
 			habit.action = _action
@@ -14,21 +15,66 @@ class HabitFactory
 		#api/twitter creation.
 		habit.action.user_id = habit.user_id if habit.action.user_id.nil?
 		habit.value.user_id = habit.user_id if habit.value and habit.value.user_id.nil?
+		
+		habit.habit_type = guess_habit_type(habit) if habit.habit_type.nil?
+
 		habit.save
 	end
 
 	def self.create_habit_from_text(text)
 		habit_text = HabitText.new(text)
-		habit = Habit.new(habit_type: habit_text.habit_type, 
-						  count: habit_text.count, 
-						  quantity: habit_text.quantity, 
-						  quantity_type: habit_text.quantity_type, 
-						  measurement: habit_text.measurement,
-						  measurement_type: habit_text.measurement_type,
-						  time: habit_text.time)
+		habit = Habit.new(
+			count: habit_text.count, 
+			quantity: habit_text.quantity, 
+			quantity_type: habit_text.quantity_type, 
+			measurement: habit_text.measurement,
+			measurement_type: habit_text.measurement_type,
+			time: habit_text.time)
 		habit.build_action(name: habit_text.action_name)
-	    habit.build_value(name: habit_text.value_name) if habit_text.value_name
-	    return habit
+		habit.build_value(name: habit_text.value_name) if habit_text.value_name
+		return habit
 	end
+
+	def self.guess_habit_type(habit)
+
+		habit_type = 0 if action_only(habit)
+		habit_type = 1 if action_and_value_only(habit)
+		habit_type = 2 if action_and_value_and_count(habit)
+		if action_and_number_and_no_value(habit)
+			habit.measurement, habit.measurement_type = habit.quantity, habit.quantity_type
+			habit.quantity, habit.quantity_type = nil, nil
+			habit_type = 3
+		end
+		habit_type = 3 if self.action_and_value_and_measurement(habit)
+		habit_type = 4 if action_and_value_and_quantity(habit)
+
+		return habit_type
+
+	end
+
+	def self.action_only habit
+		habit.action.present? and habit.count.nil? and habit.quantity.nil? and habit.quantity_type.nil? and habit.value.nil?
+	end
+
+	def self.action_and_value_only habit
+		habit.action.present? and habit.value.present? and habit.count.nil? and habit.quantity.nil? and habit.quantity_type.nil?
+	end
+
+	def self.action_and_value_and_count habit
+		habit.action.present? and habit.value.present? and habit.count.present? and habit.quantity.nil? and habit.quantity_type.nil?
+	end
+
+	def self.action_and_value_and_measurement habit
+		habit.action.present? and habit.value.nil? and habit.quantity.present?
+	end
+
+	def self.action_and_number_and_no_value habit
+		habit.action.present? and habit.value.nil? and habit.count.present? and habit.quantity.nil? and habit.quantity_type.nil?
+	end
+
+	def self.action_and_value_and_quantity habit
+		habit.action.present? and habit.value.present? and habit.quantity.present? and habit.quantity_type.present?
+	end
+
 
 end
--- app/models/concerns/habit_text.rb
+++ app/models/concerns/habit_text.rb
@@ -1,8 +1,9 @@
 class HabitText
-	attr_accessor :action_name, :count, :quantity, :quantity_type, :value_name, :measurement, :measurement_type, :habit_type, :time
+	attr_accessor :action_name, :count, :quantity, :quantity_type, :value_name, :measurement, :measurement_type, :time
+	
 	def initialize(text)
 		habit_parse_regex = 
-		/(\w+)(?:\s+(\d+)?(?:\s)*(?:(\d+)([a-zA-Z]+)(?:\s)*)?([\w\s]+)*)?(?:@)([\w\s]+)/
+		/(\w+)(?:\s+(\d+)?(?:\s)*(?:(\d+)([a-zA-Z]+)(?:\s)*)?([\w\s]+)*)?(?:(?:@)([\w\s]+))?/
 		@action_name, @count, @quantity, @quantity_type, @value_name, @time = 
 		*text.scan(habit_parse_regex).flatten
 		@value_name = @value_name.singularize if @value_name.present?
@@ -10,35 +11,5 @@ class HabitText
 		@quantity = @quantity.to_f if @quantity.present?
 		@time = Chronic.parse(@time) || Time.now
 
-		@habit_type = 0 if self.action_only?
-		@habit_type = 1 if self.action_and_value_only?
-		@habit_type = 2 if self.action_and_value_and_count?
-		if self.action_and_number_and_no_value?
-			@measurement, @measurement_type = @quantity, @quantity_type
-			@quantity, @quantity_type = nil, nil
-			@habit_type = 3
-		end
-		@habit_type = 4 if self.action_and_value_and_quantity?
 	end
-
-	def action_only?
-		@action_name.present? and @count.nil? and @quantity.nil? and @quantity_type.nil? and @value_name.nil?
-	end
-
-	def action_and_value_only?
-		@action_name.present? and @value_name.present? and @count.nil? and @quantity.nil? and @quantity_type.nil?
-	end
-
-	def action_and_value_and_count?
-		@action_name.present? and @value_name.present? and @count.present? and @quantity.nil? and @quantity_type.nil?
-	end
-
-	def action_and_number_and_no_value?
-		@action_name.present? and @value_name.nil? and @count.present? and @quantity.nil? and @quantity_type.nil?
-	end
-
-	def action_and_value_and_quantity?
-		@action_name.present? and @value_name.present? and @quantity.present? and @quantity_type.present?
-	end
-
 end
